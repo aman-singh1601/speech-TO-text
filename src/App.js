b0ab151362553d6acc03d94bdd3c13b92b2d6def
Aman <aman16singh2001@gmail.com>
2023-06-09 20:04:02 +0530

    done with react part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import useClipboard from "react-use-clipboard"
 
+import './style.css'
 function App() {
+    const StartListening=()=>SpeechRecognition.startListening({ continuous: true ,language:'en-IN'})
+    const StopListening=()=>SpeechRecognition.stopListening();
+
+    const [text,setText]=useState('');
+    const [isCopied, setCopied] = useClipboard(text)
+
+    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition()
+
+    if (!browserSupportsSpeechRecognition) {
+        return null
+      }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div className='top-container'>
+      <div className='container'>
+        <h2 className='heading'>Speech To Text Converter</h2>
+        <div className='main-content' onClick={()=>setText(transcript)}>
+            {
+                transcript
+            }
+        </div>
+        <div className='btn-style'>
+        <button class="button-28" onClick={setCopied} role="button">
+        {isCopied ? "Copied" : "Copy to Clipboard"}
+        </button>
+        <button class="button-28" onClick={StartListening} role="button">Start</button>
+        <button class="button-28" onClick={StopListening} role="button">Stop</button>
+
+        </div>
+        
+        </div>
     </div>
-  );
+
+   
+  )
 }
 
-export default App;
+export default App
